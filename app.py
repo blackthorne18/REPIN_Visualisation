e8e73f0e85c76fcf2cd8cb4a4df0eed9eb244dc7
Prajwal Bharadwaj <prajwal17@iiserb.ac.in>
2020-04-06 17:14:00 +0530

    Search bar and Tree Frame

--- app.py
+++ app.py
@@ -3,22 +3,30 @@ from tkinter import filedialog, Text
 import os,time
 
 root=tk.Tk()
+root.title("REPINs Through Time")
 blue="#0F97EF"
 white="#FFFFFF"
 
 def main():
-    canvas = tk.Canvas(root,height=700,width=900,bg="white")
+    canvas = tk.Canvas(root,height=500,width=900,bg="white")
     canvas.pack()
     
-    frame = tk.Frame(root, bg=blue)
-    frame.place(relwidth=0.35,relheight=1,relx=0)
     
-    go_button = tk.Button(root,text="Generate Tree",padx=10,pady=5,fg=blue,bg=blue)
-    go_button.pack()
-    go_button.place(x=100,y=500)
+    s_frame = tk.Frame(root, bg=blue)
+    s_frame.place(relwidth=0.35,relheight=1,relx=0)
+    
+    slab=tk.Label(s_frame,text="Search REPIN by location")
+    slab.place(relx=0.1,rely=0.04,relwidth=0.7,relheight=0.05)
+    
+    sbox=tk.Entry(s_frame,font=40)
+    sbox.place(relx=0.1,rely=0.1,relwidth=0.7,relheight=0.05)
+    
+    tree_frame= tk.Frame(root,highlightbackground=blue,highlightcolor=blue, highlightthickness=2)
+    tree_frame.place(relx=0.4,rely=0.05,relheight=0.9,relwidth=0.55)
+    
+    go_button = tk.Button(s_frame,text="Generate Tree",font=40)
+    go_button.place(relx=0.25,rely=0.75)
 
 if __name__ == '__main__':
-    st=time.time()
     main()
     root.mainloop()
-    print("Runtime: {:0.3}s".format((time.time())-st))
