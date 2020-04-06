bda748bd1ca744035938e0b84887e29ed4b30ccf
Prajwal Bharadwaj <prajwal17@iiserb.ac.in>
2020-04-06 16:28:48 +0530

    Added button

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ import os,time
 
 root=tk.Tk()
 blue="#0F97EF"
-
+white="#FFFFFF"
 
 def main():
     canvas = tk.Canvas(root,height=700,width=900,bg="white")
     canvas.pack()
+    
     frame = tk.Frame(root, bg=blue)
     frame.place(relwidth=0.35,relheight=1,relx=0)
     
-    
-    
+    go_button = tk.Button(root,text="Generate Tree",padx=10,pady=5,fg=blue,bg=blue)
+    go_button.pack()
+    go_button.place(x=100,y=500)
+
 if __name__ == '__main__':
     st=time.time()
     main()
